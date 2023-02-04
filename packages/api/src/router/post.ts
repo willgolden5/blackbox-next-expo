4316fc99fe1640ffc060da4dad5ed5fa54d7a6a9
William Golden <wtgolden5@gmail.com>
2023-02-04 18:48:10 -0500

    bug fix

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -3,14 +3,21 @@ import { z } from "zod";
 
 export const postRouter = router({
   all: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+    return ctx.prisma.user.findMany();
   }),
   byId: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-    return ctx.prisma.post.findFirst({ where: { id: input } });
+    return ctx.prisma.user.findFirst({ where: { email: input } });
   }),
-  create: protectedProcedure
-    .input(z.object({ title: z.string(), content: z.string() }))
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.post.create({ data: input });
-    }),
+  // create: protectedProcedure
+  //   .input(
+  //     z.object({
+  //       email: z.string(),
+  //       firstName: z.string(),
+  //       lastName: z.string(),
+  //       phone: z.string(),
+  //     }),
+  //   )
+  //   .mutation(({ ctx, input }) => {
+  //     return ctx.prisma.user.create(...input);
+  //   }),
 });
